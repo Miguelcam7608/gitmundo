c300deb35335c13fe4418e497f7eec87750118da
091811101-Miguel <mgacd.7608@gmail.com>
2021-06-14 20:54:37 -0500

    Primer screenshot del proyecto gitmundo 091811101

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>INDEX - MIGUEL 091811101</title>
+</head>
+<body>
+	<marquee><h1 style="font-family: Segoe UI;">¡HOLA, GITMUNDO! Soy Miguel Camacho</h1></marquee>
+</body>
+</html>
